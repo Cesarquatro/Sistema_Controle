3ea6229e7b50780f2731693924aa1c698c676efc
Cesar Augusto Silva <46902878+Cesarquatro@users.noreply.github.com>
2023-10-01 19:31:12 -0300

    ex3

---
+++ Lista_2_SCON_02_10/ex3.m
@@ -0,0 +1,31 @@
+close all; clc; clear;
+%% Lista 2 - Ex 3
+% Considere a equação diferencial:
+%        y'' + 4y' + 3y = u
+% em que y(0) = y'(0) = 0 e u(t) é um degrau unitário. 
+% Determine a solução y(t) analiticamente e verifique-a 
+% traçando em um gráfico a solução analítica e a resposta 
+% ao degrau obtida com a função step.
+% (figura) 
+
+num = 1; 
+den = [1 4 3];
+sys = tf(num, den)
+
+t = 0:0.1:10;
+ut = step(sys, t); % degrau unitário
+yt = 1/3 - 1/2*exp(-t) + 1/6*exp(-3*t);
+
+%plot
+plot(t, ut,'-', 'Color', 'r', 'LineWidth', 8)
+hold on
+plot(t, yt, '--', 'Color', 'c', 'LineWidth', 2)
+
+grid on;
+
+title('Verificação');
+xlabel('Tempo [s]');
+ylabel('Amplitude');
+legend('Resposta do sistema ao degrau unitário u(t)', ...
+    'Solução y(t) obtida analiticamente', ...
+    'Location','southeast')
